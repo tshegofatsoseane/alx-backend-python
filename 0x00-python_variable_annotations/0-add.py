aca3746e870517f49a551fd95d3d7c75fc42a86b
tshegofatsoseane <tshegofatsoseane@gmail.com>
2024-03-15 02:10:57 +0000

    Initial commit

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+'''
+    Basic annotations for variables. Script
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+        Return sum of two floats.
+    '''
+    return a + b
